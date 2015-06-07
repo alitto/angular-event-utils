e5241a8b09124e4999024ba43bad48ddeb409d84
alitto <alitto@gmail.com>
2015-06-06 21:50:51 -0300

    Setup package.json

---
+++ examples/directives/login-form.js
@@ -0,0 +1,11 @@
+
+/*
+ * Login form component
+ */
+angular.module('myApp.loginForm', [])
+.directive('loginForm', [function() {
+	return {
+		restrict: 'E',
+		templateUrl: 'views/login-form.html?123222'
+	};
+}]);
---
+++ examples/view1/view1.js
@@ -0,0 +1,69 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute', 'myApp.loginForm'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  });
+}])
+
+.controller('View1Ctrl', ['$scope', function($scope) {
+
+	var items = [
+		'Ale',
+		'Jose',
+		'Benito',
+		'Bale'
+	];
+
+	$scope.items = [];
+
+	// Trigger service from events instead of exposing interface directly to the view
+	$scope.$on('greet user', function(e, data){
+
+		alert('Hello user!');
+	});
+
+	$scope.$on('sort', function(e, data){
+
+		$scope.items.sort();
+	});
+
+	$scope.$on('reset', function(e, data){
+
+		$scope.items = angular.copy(items);
+	});
+
+	$scope.$on('remove', function(e, index){
+
+		// Remove item in the given position
+		$scope.items.splice(index, 1);
+	});
+
+	$scope.$on('add', function(e){
+
+		// Add new item
+		$scope.items.push($scope.newItem);
+
+		$scope.newItem = ''; // Reset
+	});
+
+	$scope.$on('login', function(e, data){
+
+		console.log('Login data', data);
+
+		setTimeout(function(){
+			if(data.password.length >= 8){
+				$scope.$broadcast('login success', true);
+			}else{
+				$scope.$broadcast('login error', {
+					errorMessage: 'Password is too short'
+				});
+			}
+		}, 2000);
+
+	});
+	
+}]);
